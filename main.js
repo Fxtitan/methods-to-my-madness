34599b86f2dcf7f29737bb7a8558276c112c713d
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-02-26 08:05:48 -0500

    sigh

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function repeat(str, times) {
+  return new Array(times + 1).join(str);
+}// (times + 1) the length of array same as str.length
+repeat('omg ', 3)
+console.log(repeat('omg ',3))
 
-
+function includes (arr, hold){
+  for (let i = 0; i < arr.length; i ++){
+    if(new Array(arr + 1).join(hold)){
+      return true;}
+  }return false;
+}
 
 
 
